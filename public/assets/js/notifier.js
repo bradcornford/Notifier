0e663d5cf32ee14a64d2438ddb01eb918a584c75
Bradley Cornford <bcornford@plus.net>
2015-12-17 10:47:10 +0000

    Updating notifier.js to supress multi-reponses.

--- public/assets/js/notifier.js
+++ public/assets/js/notifier.js
@@ -1,12 +1,13 @@
-/** Notifier.js - v1.1.0 - 2015/11/26
+/** Notifier.js - v1.1.0 - 2015/17/12
  * https://github.com/bradcornford/Notifier
  * Copyright (c) 2015 Bradley Cornford - MIT
  */
+
 (function ($) {
 
     $.notify.addStyle('bootstrap',{html:'<div class="alert" role="alert">\n<span data-notify-text></span>\n</div>',classes:{base:{padding:'15px','margin-bottom':'20px',border:'1px solid transparent','border-radius':'4px',color:'#545454','background-color':'#e8e8e8','border-color':'#d1d1d1'},error:{color:'#a94442','background-color':'#f2dede','border-color':'#ebccd1'},success:{color:'#3c763d','background-color':'#dff0d8','border-color':'#d6e9c6'},info:{color:'#31708f','background-color':'#d9edf7','border-color':'#bce8f1'},warn:{color:'#8a6d3b','background-color':'#fcf8e3','border-color':'#faebcc'}}});
 
-    var notifierUrl, notifierLoad, notifierSettings;
+    var notifierUrl, notifierLoad, notifierSettings, notifierTimestamp = 0;
 
     $.fn.notifier = function(options) {
         var settings = $.extend({
@@ -35,9 +36,10 @@
             notifierUrl = $(location).attr('protocol') + '//' + $(location).attr('hostname') + '/notifier/notifications';
         }
 
-        notifierDisplay = function (settings) {
+        notifierSettings = settings;
+
+        notifierDisplay = function (settings, url) {
             var display = false;
-            var url = settings.url;
 
             if (notifierSettings.enabled && notifierSettings.ajaxLoad && url != notifierUrl) {
                 display = true;
@@ -68,21 +70,35 @@
             );
         }
 
-        if (settings.enabled && settings.pageLoad) {
+        if (notifierSettings.enabled && notifierSettings.pageLoad) {
             notifierLoad();
         }
 
-        notifierSettings = settings;
-
         $(document).ajaxComplete(function(evt, xhr, settings) {
-            notifierDisplay(settings);
+            if (notifierTimestamp == 0) {
+                notifierTimestamp = evt.timestamp;
+            }
+
+            if (notifierTimestamp != evt.timestamp) {
+                notifierDisplay(settings, settings.url);
+            }
         });
 
         $(document).on(
             'bootstrap-ajax:success bootstrap-ajax:error',
-            function (e)
+            function (evt, settings)
             {
-                notifierDisplay(settings);
+                var url;
+
+                notifierTimestamp = evt.timestamp;
+
+                if (settings.context.href != undefined) {
+                    url = settings.context.href;
+                } else {
+                    url = settings.context.action;
+                }
+
+                notifierDisplay(settings, url);
             }
         );
 
